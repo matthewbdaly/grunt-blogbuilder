835c80870f9b34aa06e521fbad270eb8b1c310dd
Matthew Daly <matthewbdaly@gmail.com>
2015-01-17 15:56:21 +0000

    Added Google Analytics support

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,7 @@ module.exports = function (grunt) {
           data: {
             author: "My Name",
             url: "http://www.example.com",
+            googleanalytics: "UA-XXXXX-X",
             facebookcomments: "",
             disqus: "",
             title: 'My new blog',
