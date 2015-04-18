6bbd302071de9bba680130ad2d98f2129b4d1840
Matthew Daly <matthewbdaly@gmail.com>
2015-04-18 12:02:24 +0100

    Removed unwanted code from task

--- tasks/blogbuilder.js
+++ tasks/blogbuilder.js
@@ -418,32 +418,6 @@ module.exports = function (grunt) {
 
     // Create robots.txt file
     grunt.file.copy(options.template.robots, options.www.dest + '/robots.txt');
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
   });
 
 };
